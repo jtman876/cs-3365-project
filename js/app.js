72ea3c3471a9ad77a7f69fbed583421a1310ee8d
jtman <jtman876@gmail.com>
2025-11-04 00:47:36 -0600

    Create homepage links, styles, and navbar

--- js/app.js
+++ js/app.js
@@ -1,6 +1,43 @@
-
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+//import { isAuthenticated } from './js/auth.js'
 
 const supabaseUrl = 'https://krjjfaendpntpjocgdbl.supabase.co'
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtyampmYWVuZHBudHBqb2NnZGJsIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTk5MDMwNDAsImV4cCI6MjA3NTQ3OTA0MH0.5YtJH_grLZRzapwH7aJEJ2yHUgCGEry28iMGuu_X1ls'
 const supabase = createClient(supabaseUrl, supabaseKey)
+
+// TODO: move to auth.js
+function isAuthenticated() {
+  return false;
+}
+
+// Build the Navigation Bar
+let nav = document.querySelectorAll('nav')[0];
+if (isAuthenticated()) {
+  let profile = document.createElement('a');
+  profile.text = 'Profile';
+  profile.href = './profile.html'
+  nav.appendChild(profile);
+} else {
+  let login = document.createElement('a');
+  login.text = 'Log in';
+  login.href = './login.html'
+  nav.appendChild(login);
+
+  let register = document.createElement('a');
+  register.text = 'Register';
+  register.href = './register.html'
+  nav.appendChild(register);
+}
+
+// Hover over links
+let links = document.querySelectorAll('section')[0].children;
+for (const link of links) {
+  link.addEventListener("mouseover", (event) => {
+    event.target.style.background = "lightblue";
+    // event.target.style.fontSize = "1.8rem";
+  })
+  link.addEventListener("mouseout", (event) => {
+    event.target.style.background = "white";
+    // event.target.style.fontSize = "1.5rem";
+  })
+}
